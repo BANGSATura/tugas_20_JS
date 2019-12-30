c9a99a2725d8ebe3d0d3972253305d9a4dfae9a2
STR <saturasstotenkopf@gmail.com>
2019-12-30 15:14:11 +0700

    Tugas 20 JS
    Mengumpulkan Tugas 20 Js

---
+++ tugas20.js
@@ -0,0 +1,6 @@
+function caridata() {
+  var data = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+  console.log(data.match(/are/));
+  console.log(data.match("words"));
+}
+caridata();
